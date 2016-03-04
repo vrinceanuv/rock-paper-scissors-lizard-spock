5e2483e52337e38ddab7ea0b3f4fe5181ae21d3e
Valentin Vrinceanu <vrinceanu.valentin@gmail.com>
2016-03-04 16:39:50 +0200

    Added all the game files

--- js/battlefield.js
+++ js/battlefield.js
@@ -4,136 +4,136 @@
 
 
 var weapons = document.querySelectorAll('.choose img'),
-	scoreElement = document.querySelector('.score'),
-	userScoreElement = document.querySelector('.user-score'),
-	computerScoreElement = document.querySelector('.computer-score'),
-	userWeaponElement = document.querySelector('.user-weapon'),
-	computerWeaponElement = document.querySelector('.computer-weapon'),
-	userWeaponImage = document.createElement('img'),
-	computerWeaponImage = document.createElement('img'),
-	userScore = 0,
-	computerScore = 0,
-	userWeapon,
-	computerWeapon,
-	finalResult,
-	weaponsAvailable = {
-		'rock': 'rock',
-		'paper': 'paper',
-		'scissors': 'scissors',
-		'lizard': 'lizard',
-		'spock': 'spock'
-	},
-	results = {
-		'tie': 'It is a tie',
-		'userWin': 'You have won',
-		'computerWin': 'You have lost'
-	};
+    scoreElement = document.querySelector('.score'),
+    userScoreElement = document.querySelector('.user-score'),
+    computerScoreElement = document.querySelector('.computer-score'),
+    userWeaponElement = document.querySelector('.user-weapon'),
+    computerWeaponElement = document.querySelector('.computer-weapon'),
+    userWeaponImage = document.createElement('img'),
+    computerWeaponImage = document.createElement('img'),
+    userScore = 0,
+    computerScore = 0,
+    userWeapon,
+    computerWeapon,
+    finalResult,
+    weaponsAvailable = {
+        'rock': 'rock',
+        'paper': 'paper',
+        'scissors': 'scissors',
+        'lizard': 'lizard',
+        'spock': 'spock'
+    },
+    results = {
+        'tie': 'It is a tie',
+        'userWin': 'You have won',
+        'computerWin': 'You have lost'
+    };
 
 
 for(var i = 0; i < weapons.length; i++) {
-	weapons[i].addEventListener('click', function(){
-		userWeapon = this.getAttribute('data-weapon');
-		userWeaponImage.src = this.getAttribute('src');
-		compare(userWeapon);
-	})
+    weapons[i].addEventListener('click', function(){
+        userWeapon = this.getAttribute('data-weapon');
+        userWeaponImage.src = this.getAttribute('src');
+        compare(userWeapon);
+    })
 }
 
 function compare(userWeapon) {
-	computerWeapon = computerChoice();
+    computerWeapon = computerChoice();
 
-	if(userWeapon === computerWeapon) {
-		finalResult = results.tie;
-		updateScores(finalResult);
-		return;
-	}
+    if(userWeapon === computerWeapon) {
+        finalResult = results.tie;
+        updateScores(finalResult);
+        return;
+    }
 
-	if(userWeapon === weaponsAvailable.rock) {
-		compareRock();
-	}
+    if(userWeapon === weaponsAvailable.rock) {
+        compareRock();
+    }
 
-	if(userWeapon === weaponsAvailable.paper) {
-		comparePaper();
-	}
+    if(userWeapon === weaponsAvailable.paper) {
+        comparePaper();
+    }
 
-	if(userWeapon === weaponsAvailable.scissors) {
-		compareScissors();
-	}
+    if(userWeapon === weaponsAvailable.scissors) {
+        compareScissors();
+    }
 
-	if(userWeapon === weaponsAvailable.lizard) {
-		compareLizard();
-	}
+    if(userWeapon === weaponsAvailable.lizard) {
+        compareLizard();
+    }
 
-	if(userWeapon === weaponsAvailable.spock) {
-		compareSpock();
-	}
+    if(userWeapon === weaponsAvailable.spock) {
+        compareSpock();
+    }
 
-	updateScores(finalResult);
+    updateScores(finalResult);
 }
 
 function compareRock() {
-	if(computerWeapon === weaponsAvailable.scissors || computerWeapon === weaponsAvailable.lizard) {
-		finalResult = results.userWin;
-	} else {
-		finalResult = results.computerWin;
-	}
+    if(computerWeapon === weaponsAvailable.scissors || computerWeapon === weaponsAvailable.lizard) {
+        finalResult = results.userWin;
+    } else {
+        finalResult = results.computerWin;
+    }
 }
 
 function comparePaper() {
-	if(computerWeapon === weaponsAvailable.rock || computerWeapon === weaponsAvailable.spock) {
-		finalResult = results.userWin;
-	} else {
-		finalResult = results.computerWin;
-	}
+    if(computerWeapon === weaponsAvailable.rock || computerWeapon === weaponsAvailable.spock) {
+        finalResult = results.userWin;
+    } else {
+        finalResult = results.computerWin;
+    }
 }
 
 function compareScissors() {
-	if(computerWeapon === weaponsAvailable.paper || computerWeapon === weaponsAvailable.lizard) {
-		finalResult = results.userWin;
-	} else {
-		finalResult = results.computerWin;
-	}
+    if(computerWeapon === weaponsAvailable.paper || computerWeapon === weaponsAvailable.lizard) {
+        finalResult = results.userWin;
+    } else {
+        finalResult = results.computerWin;
+    }
 }
 
 function compareLizard() {
-	if(computerWeapon === weaponsAvailable.paper || computerWeapon === weaponsAvailable.spock) {
-		finalResult = results.userWin;
-	} else {
-		finalResult = results.computerWin;
-	}
+    if(computerWeapon === weaponsAvailable.paper || computerWeapon === weaponsAvailable.spock) {
+        finalResult = results.userWin;
+    } else {
+        finalResult = results.computerWin;
+    }
 }
 
 function compareSpock() {
-	if(computerWeapon === weaponsAvailable.rock || computerWeapon === weaponsAvailable.scissors) {
-		finalResult = results.userWin;
-	} else {
-		finalResult = results.computerWin;
-	}
+    if(computerWeapon === weaponsAvailable.rock || computerWeapon === weaponsAvailable.scissors) {
+        finalResult = results.userWin;
+    } else {
+        finalResult = results.computerWin;
+    }
 }
 
 function updateScores(finalResult) {
-	if (finalResult === results.userWin) {
-		userScore++;
-		userScoreElement.innerHTML = userScore;
-	}
-	if (finalResult === results.computerWin) {
-		computerScore++;
-		computerScoreElement.innerHTML = computerScore;
-	}
-	userWeaponElement.appendChild(userWeaponImage);
-	computerWeaponElement.appendChild(computerWeaponImage);
-	scoreElement.innerHTML = finalResult;
+    if (finalResult === results.userWin) {
+        userScore++;
+        userScoreElement.innerHTML = userScore;
+    }
+    if (finalResult === results.computerWin) {
+        computerScore++;
+        computerScoreElement.innerHTML = computerScore;
+    }
+    userWeaponElement.appendChild(userWeaponImage);
+    computerWeaponElement.appendChild(computerWeaponImage);
+    scoreElement.innerHTML = finalResult;
 }
 
 function computerChoice() {
-	var counter = 0,
-		keys = Object.keys(weaponsAvailable),
-		computerWeapon = weaponsAvailable[keys[keys.length * Math.random() << 0]]
+    var counter = 0,
+        keys = Object.keys(weaponsAvailable),
+        computerWeapon = weaponsAvailable[keys[keys.length * Math.random() << 0]]
 
     for(var i = 0; i < weapons.length; i++) {
-		if(weapons[i].getAttribute('data-weapon') === computerWeapon) {
-			computerWeaponImage.src = weapons[i].getAttribute('src');
-		}
-	}
+        if(weapons[i].getAttribute('data-weapon') === computerWeapon) {
+            computerWeaponImage.src = weapons[i].getAttribute('src');
+        }
+    }
 
     return computerWeapon;
 }
